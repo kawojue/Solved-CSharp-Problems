0c0b6d27f197a14c338d017fbbc6aedea862f05b
Raheem Kawojue <kawojue08@gmail.com>
2023-06-19 14:42:20 +0100

    [add] Q13

---
+++ Q13/Q13/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AddFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = Add(1, 2, 3, 4, 5);
+            Console.WriteLine(sum);
+        }
+        public static int Add(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+    }
+}

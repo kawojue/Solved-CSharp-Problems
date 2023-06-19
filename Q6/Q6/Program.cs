5d558002c88132960d908c2938794ed169e33cd1
Raheem Kawojue <kawojue08@gmail.com>
2023-06-19 13:30:03 +0100

    [add] Q6

---
+++ Q6/Q6/Program.cs
@@ -0,0 +1,25 @@
+﻿public class SumTriple
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Num 1: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Num 2: ");
+        int b = int.Parse(Console.ReadLine());
+
+        int sum = Sum(a, b);
+
+        if (a == b)
+        {
+            sum *= 3;
+        }
+
+        Console.WriteLine("The sum of the two numbers is: " + sum);
+    }
+
+    public static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+}

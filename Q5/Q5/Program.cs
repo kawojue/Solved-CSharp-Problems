2a751f97f844fc76960ca3871e0f3232411e1a27
Raheem Kawojue <kawojue08@gmail.com>
2023-06-19 13:15:46 +0100

    [add] q5

---
+++ Q5/Q5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Dcoder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int temp;
+            int first;
+            int second;
+
+            Console.Write("First number: ");
+            first = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Second number: ");
+            second = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Before Swapping:\n\tfirst = " + first + "\n\tsecond= " + second);
+
+            temp = first;
+            first = second;
+            second = temp;
+
+            Console.WriteLine("After Swapping:\n\tfirst = " + first + "\n\tsecond= " + second);
+        }
+    }
+}

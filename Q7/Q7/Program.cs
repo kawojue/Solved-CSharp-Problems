01abea4da40dbb41372718c4aa68beb321ca4bc1
Raheem Kawojue <kawojue08@gmail.com>
2023-06-19 13:40:11 +0100

    [add] gitignore

---
+++ Q7/Q7/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Convert
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Celcius Degree: ");
+        int c = int.Parse(Console.ReadLine());
+        int k = ConvertToKelvin(c);
+        int f = ConvertToFahrenheit(c);
+
+        Console.WriteLine("Kelvin = " + k + "\nFahrenheit = " + f);
+    }
+
+    public static int ConvertToKelvin(int c)
+    {
+        return c + 273;
+    }
+    public static int ConvertToFahrenheit(int c)
+    {
+        return c * 18 / 10 + 32;
+    }
+}

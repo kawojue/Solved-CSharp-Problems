d4ec62be4b7744d58d536c2d444fb341c0f00115
Raheem Kawojue <kawojue08@gmail.com>
2023-06-19 14:42:13 +0100

    [add] Q12

---
+++ Q12/Q12/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("String: ");
+            string inputString = Console.ReadLine();
+
+            bool isPalindrome = IsPalindrome(inputString);
+            Console.WriteLine(isPalindrome);
+        }
+
+        public static bool IsPalindrome(string inputString)
+        {
+            char[] chars = inputString.ToCharArray();
+
+            int start = 0;
+            int end = chars.Length - 1;
+
+            while (start < end)
+            {
+                if (chars[start] != chars[end])
+                {
+                    return false;
+                }
+
+                start++;
+                end--;
+            }
+
+            return true;
+        }
+    }
+}

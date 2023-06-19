afc15f1f2ef927176e6d88d60ea1f93ea1c21354
Raheem Kawojue <kawojue08@gmail.com>
2023-06-19 12:00:56 +0100

    [add] q3

---
+++ Q3/Q3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Q3 — Stack
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a sequence of characters: ");
+        string input = Console.ReadLine();
+
+        string reversed = ReverseString(input);
+        Console.WriteLine("Reversed string: " + reversed);
+    }
+
+    static string ReverseString(string input)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char c in input)
+        {
+            stack.Push(c);
+        }
+
+        string reversed = "";
+        while (stack.Count > 0)
+        {
+            reversed += stack.Pop();
+        }
+
+        return reversed;
+    }
+}

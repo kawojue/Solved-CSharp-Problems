c21d5f787f52b6efde7bc09d88735e9854046ca5
Raheem Kawojue <kawojue08@gmail.com>
2023-06-19 14:00:56 +0100

    [add] Q0

---
+++ Q9/Q9/Program.cs
@@ -0,0 +1,19 @@
+﻿public class SquareRoot
+{
+    public static void Main(string[] args)
+    {
+        double number;
+
+        try
+        {
+            Console.Write("Enter a real number: ");
+            number = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("The square root of {0} is {1}", number, Math.Sqrt(number));
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input. Please enter a valid real number.");
+        }
+    }
+}

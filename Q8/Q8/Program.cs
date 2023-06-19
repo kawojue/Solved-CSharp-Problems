78993510a503a661ecbc27ab91d5eb8f848977e7
Raheem Kawojue <kawojue08@gmail.com>
2023-06-19 13:57:13 +0100

    [add] Q8

---
+++ Q8/Q8/Program.cs
@@ -0,0 +1,19 @@
+﻿public class NumberKeyPress
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Press a number key from 0 to 9: ");
+        char key = Console.ReadKey().KeyChar;
+
+        Console.WriteLine("\n");
+
+        if (key >= '0' && key <= '9')
+        {
+            Console.WriteLine("You pressed: " + key);
+        }
+        else
+        {
+            Console.WriteLine("Not allowed");
+        }
+    }
+}

2c8993a5f4733aec83da712b3ce5c59ce9184d8f
Raheem Kawojue <kawojue08@gmail.com>
2023-06-21 02:56:59 +0100

    [add] Q19

---
+++ Q19/Q19/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace CopyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the path of the source file: ");
+            string sourceFileName = Console.ReadLine();
+
+            Console.Write("\nEnter the path of the destination file: ");
+            string destFileName = Console.ReadLine();
+
+            // Read the content of the source file.
+            string content = File.ReadAllText(sourceFileName);
+
+            // Write the content to the destination file.
+            File.WriteAllText(destFileName, content);
+
+            Console.WriteLine("\n\nFile copied successfully.");
+        }
+    }
+}
